2eaef386008434c33c5e0b5cc3ebc74f5a6ef7d9
Darya Shashko <darya_shashko@epam.com>
2022-01-30 19:15:54 +0200

    feat: client is running

---
+++ src/client/webpack.dev.ts
@@ -0,0 +1,48 @@
+import path from 'path';
+import { Configuration, DefinePlugin } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
+const webpackConfig = (): Configuration => ({
+    entry: path.join(__dirname, '/src/index.tsx'),
+    ...(process.env.production || !process.env.development ? {} : { devtool: 'eval-source-map' }),
+
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+        plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })]
+    },
+    output: {
+        path: path.join(__dirname, '/build'),
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles
+            template: './public/index.html'
+        }),
+        // DefinePlugin allows you to create global constants which can be configured at compile time
+        new DefinePlugin({
+            'process.env': process.env.production || !process.env.development
+        }),
+        new ForkTsCheckerWebpackPlugin()
+    ]
+});
+
+export default webpackConfig;
