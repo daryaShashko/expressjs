96556614ac129a42a6f30c3484e6a9c0b67870ad
Darya Shashko <darya_shashko@epam.com>
2022-01-30 19:35:36 +0200

    feat: add mui for client

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link, Paper, Box } from '@mui/material';
 
 // eslint-disable-next-line react/function-component-definition
 const App: React.FC = () => (
     <div className="wrapper">
         <h1>React 17 and TypeScript 4 App!🚀</h1>
+        <Link href="#">Link</Link>
+        <Box
+            sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                '& > :not(style)': {
+                    m: 1,
+                    width: 128,
+                    height: 128
+                }
+            }}>
+            <Paper />
+            <Paper elevation={3} />
+        </Box>
     </div>
 );
 export default App;
