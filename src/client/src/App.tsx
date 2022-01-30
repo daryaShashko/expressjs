0d2a62cd7d11fc8d4e54bf00d383221921a82d91
Darya Shashko <darya_shashko@epam.com>
2022-01-30 22:44:03 +0200

    feat: add listener for 'api/quotes/'

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import { Link, Paper, Box } from '@mui/material';
+import { Link, Paper, Box, Button } from '@mui/material';
 
 // eslint-disable-next-line react/function-component-definition
-const App: React.FC = () => (
-    <div className="wrapper">
-        <h1>React 17 and TypeScript 4 App!🚀</h1>
-        <Link href="#">Link</Link>
-        <Box
-            sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                '& > :not(style)': {
-                    m: 1,
-                    width: 128,
-                    height: 128
-                }
-            }}>
-            <Paper />
-            <Paper elevation={3} />
-        </Box>
-    </div>
-);
+const App: React.FC = () => {
+    const getAllQuotes = async () => {
+        const response = await fetch('/api/quotes/', {
+            method: 'GET' // *GET, POST, PUT, DELETE, etc.
+        });
+        const parsedResponse = await response.json();
+        console.log(parsedResponse);
+        // setQuotes(parsedResponse);
+    };
+
+    return (
+        <div className="wrapper">
+            <h1>React 17 and TypeScript 4 App!🚀</h1>
+            <Link href="#">Link</Link>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    '& > :not(style)': {
+                        m: 1,
+                        width: 128,
+                        height: 128
+                    }
+                }}>
+                <Paper />
+                <Paper elevation={3} />
+            </Box>
+            <Button onClick={getAllQuotes}>Get all quotes</Button>
+        </div>
+    );
+};
 export default App;
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import path from 'path';
 import { webpack } from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackConfig from '../client/webpack.dev';
+import fs from 'fs';
+import { resolve } from 'path';
 // rest of the code remains same
 const app = express();
 const PORT = 3000;
@@ -17,7 +20,19 @@ app.use(
 );
 
 // Serve the files on port 3000.
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+// app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.get('/api/quotes', (req, res) => {
+    const quotesReadableStream = fs.createReadStream(path.resolve(process.cwd(), 'quotes.json'), { encoding: 'utf8' });
+    quotesReadableStream.pipe(res);
+    quotesReadableStream.on('error', () => {
+        res.writeHead(500);
+        res.end('Bad response');
+    });
+});
+
+// app.get('/users/:userId', (req, res) => {
+//     return res.send(users[req.params.userId]);
+// });
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
